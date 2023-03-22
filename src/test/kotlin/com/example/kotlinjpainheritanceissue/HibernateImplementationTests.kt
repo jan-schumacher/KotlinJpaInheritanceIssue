22ed928d42d565bdc1a62bc0f577fc8ea5c38529
Jan Schumacher <j.schumacher@eos-ts.com>
2023-03-22 08:07:34 +0100

    Prove that spring data works fine

--- src/test/kotlin/com/example/kotlinjpainheritanceissue/HibernateImplementationTests.kt
+++ src/test/kotlin/com/example/kotlinjpainheritanceissue/HibernateImplementationTests.kt
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository
+import java.util.*
 
 @SpringBootTest
 class HibernateImplementationTests {
@@ -14,15 +16,68 @@ class HibernateImplementationTests {
     lateinit var entityManaFactory: EntityManagerFactory
 
     @Test
-    fun contextLoads() {
+    fun testWithSameEntityManager() {
         val sessionFactory = entityManaFactory.unwrap(SessionFactory::class.java)
+        val entity2 = Entity2("value1Example", "value2Example")
+        val em = entityManaFactory.createEntityManager()
+        em.transaction.begin()
+
+        em.persist(entity2)
+        em.transaction.commit()
+
+        val simpleJpaRepository = SimpleJpaRepository<Entity2, UUID>(Entity2::class.java, em)
+
+        val result = simpleJpaRepository.getReferenceById(entity2.id)
+        Assertions.assertEquals("value1Example", result.value1)
+        Assertions.assertEquals("value2Example", result.value2)
+    }
+
+    @Test
+    fun testWithNewEntityManager() {
+        val sessionFactory = entityManaFactory.unwrap(SessionFactory::class.java)
+        val entity2 = Entity2("value1Example", "value2Example")
+        val em = entityManaFactory.createEntityManager()
+        em.transaction.begin()
+
+        em.persist(entity2)
+        em.transaction.commit()
+
+        val simpleJpaRepository = SimpleJpaRepository<Entity2, UUID>(Entity2::class.java, sessionFactory.createEntityManager())
+
+        val result = simpleJpaRepository.getReferenceById(entity2.id)
+        Assertions.assertEquals("value1Example", result.value1)
+        Assertions.assertEquals("value2Example", result.value2)
+    }
+
+    @Test
+    fun testLoadWithSameSession() {
+        val sessionFactory = entityManaFactory.unwrap(SessionFactory::class.java)
+
         val session = sessionFactory.openSession()
         session.beginTransaction()
+
         val entity2 = Entity2("value1Example", "value2Example")
         session.persist(entity2)
         session.transaction.commit()
 
         val result = session.get(Entity2::class.java, entity2.id)
+        Assertions.assertEquals("value1Example", result.value1)
+        Assertions.assertEquals("value2Example", result.value2)
+    }
+
+    @Test
+    fun testLoadWithNewSession() {
+        val sessionFactory = entityManaFactory.unwrap(SessionFactory::class.java)
+
+        val session = sessionFactory.openSession()
+        session.beginTransaction()
+
+        val entity2 = Entity2("value1Example", "value2Example")
+        session.persist(entity2)
+        session.transaction.commit()
+
+        val newSession = sessionFactory.openSession()
+        val result = newSession.get(Entity2::class.java, entity2.id)
         Assertions.assertEquals("value1Example", result.value1)
         Assertions.assertEquals("value2Example", result.value2)
     }
